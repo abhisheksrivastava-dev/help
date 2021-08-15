cf4b90b6f0fe2d37655b889a4f8bf5136ff9e889
Abhishek Srivastava <74584768+abhisheksrivastava-dev@users.noreply.github.com>
2021-08-16 00:32:23 +0530

    Create findIndexOfSubstring.js

---
+++ findIndexOfSubstring.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    if(haystack === '' && needle === '') return 0
+    else if(haystack.includes(needle)){
+        return haystack.indexOf(needle);
+    } else {
+        return -1;
+    }
+};
