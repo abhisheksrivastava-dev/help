f795ee770b8f4da9e3051b5d9f84484e5441bc70
Abhishek Srivastava <74584768+abhisheksrivastava-dev@users.noreply.github.com>
2021-08-25 08:44:24 +0530

    Create climbingStairs.js

---
+++ climbingStairs.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    /**
+     * Recursion
+    if(n === 1) {
+        return 1;
+    } else if(n === 2) {
+        return 2;
+    } else {
+        return climbStairs(n-1) + climbStairs(n-2);
+    }
+    */
+    let prev = 0;
+    let cur = 1;
+    let temp;
+    
+    for (let i = 0; i < n; i++) {
+        temp = prev;
+        prev = cur;
+        cur += temp; 
+    }
+    return cur;
+};
